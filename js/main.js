1a4ad4faf08a920a7f0452f7f1e8f3e2c639dc90
Reshma D <developerreshmad@gmail.com>
2018-11-08 00:05:57 +0530

    final register service worker works-imp 5.0

--- js/main.js
+++ js/main.js
@@ -4,17 +4,13 @@ let restaurants,
 var newMap
 var markers = []
 
-//google ver
+//MY VERSION
 // Register service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/sw.js').then(function(reg) {
+    console.log('ServiceWorker registration successful');
+  }).catch(function(err) {
+    console.log('ServiceWorker registration failed', err);
   });
 }
 /**
