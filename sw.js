6a58b98b882a4d3751518583aff5015078d773d8
Reshma D <developerreshmad@gmail.com>
2018-11-09 00:36:43 +0530

    final sw with commnets works-7.0

--- sw.js
+++ sw.js
@@ -6,13 +6,13 @@ var allCaches = [
     contentImgsCache
 ];
 
-//2
-/** At Service Worker Install time, cache all static assets */
+//TODO: Install a service worker- Open a cache named "Restaurant-Reviews-v1.0"
+//TODO: Cache all static assets - add to cache, the following urls
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
             return cache.addAll([
-                '/', // this caches index.html
+                '/', // alias for index.html
                 '/restaurant.html',
                 '/css/styles.css',
                 '/css/mediaSmall.css',
@@ -31,8 +31,8 @@ self.addEventListener('install', function (event) {
     );
 });
 
-//3
-/** At Service Worker Activation, Delete previous caches, if any */
+//TODO: Activate Service Worker
+//TODO: Delete previous caches, if any
 self.addEventListener('activate', function (event) {
     event.waitUntil(
         caches.keys().then(function (cacheNames) {
@@ -48,29 +48,33 @@ self.addEventListener('activate', function (event) {
     );
 });
 
-//4
-/** Hijack fetch requests and respond accordingly */
+//TODO: ServiceWoker create Custom responses to fetch requests
 self.addEventListener('fetch', function (event) {
     const requestUrl = new URL(event.request.url);
 
-    // only highjack request made to our app (not mapbox maps or leaflet, for example)
     if (requestUrl.origin === location.origin) {
-
-        // Since requests made to restaurant.html have search params (like ?id=1), the url can't be used as the
-        // key to access the cache, so just respondWith restaurant.html if pathname startsWith '/restaurant.html'
+        /**
+         * TODO:respond to requests for the root page with
+         * the page from the cache ignore the search params
+         * just respondWith restaurant.html if pathname startsWith '/restaurant.html'
+         */
         if (requestUrl.pathname.startsWith('/restaurant.html')) {
             event.respondWith(caches.match('/restaurant.html'));
-            return; // Done handling request, so exit early.
+            return;
         }
 
         // If the request pathname starts with /img, then we need to handle images.
         if (requestUrl.pathname.startsWith('/img')) {
             event.respondWith(serveImage(event.request));
-            return; // Done handling request, so exit early.
+            return;
         }
     }
-
-    // Default behavior: respond with cached elements, if any, falling back to network.
+    /**
+     * TODO: respond with an entry from cache if there is one,ignore the search params
+     * if there isn't an entry in cache,fetch from network,cache a clone, then return network response
+     * TODO: provide a default fallback if network is not available or offline
+     * ref-https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers#The_premise_of_service_workers
+     */
     event.respondWith(
         caches.match(event.request,{ignoreSearch:true}).then(function(resp) {
             return resp || fetch(event.request).then(function(response) {
@@ -89,19 +93,20 @@ self.addEventListener('fetch', function (event) {
 });
 
 
-
-
+/**
+ * TODO: cache Responsive images
+ * reference-cache images
+ * https://classroom.udacity.com/courses/ud899/lessons/6381510082/concepts/63774101810923
+ */
 function serveImage(request) {
     let imageStorageUrl = request.url;
 
-    // Make a new URL with a stripped suffix and extension from the request url
-    // i.e. /img/1-medium.jpg  will become  /img/1
-    // we'll use this as the KEY for storing image into cache
     imageStorageUrl = imageStorageUrl.replace(/-small\.\w{3}|-medium\.\w{3}|-large\.\w{3}/i, '');
 
     return caches.open(contentImgsCache).then(function (cache) {
         return cache.match(imageStorageUrl).then(function (response) {
-            // if image is in cache, return it, else fetch from network, cache a clone, then return network response
+            //TODO: respond with an image entry from cache if there is one,
+            //if there isn't an image entry in cache,fetch from network,cache a clone, then return network response
             return response || fetch(request).then(function (networkResponse) {
                 cache.put(imageStorageUrl, networkResponse.clone());
                 return networkResponse;
